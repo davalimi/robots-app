7517256fc19d30d01fd478c1a21d466f9ad5b592
david alimi <davealimi@gmail.com>
2020-02-20 12:42:30 +0200

    Updates

---
+++ precache-manifest.cb761952823dc1a0ca7f7bb42854ce67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e2c2c8aa00451c08995343158db46f",
+    "url": "/robots-app/index.html"
+  },
+  {
+    "revision": "4c981857c343a07a8ed6",
+    "url": "/robots-app/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "b89275fb80fe52f92f2d",
+    "url": "/robots-app/static/css/main.752d6d46.chunk.css"
+  },
+  {
+    "revision": "4c981857c343a07a8ed6",
+    "url": "/robots-app/static/js/2.f5edd3b6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robots-app/static/js/2.f5edd3b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b89275fb80fe52f92f2d",
+    "url": "/robots-app/static/js/main.a05016fa.chunk.js"
+  },
+  {
+    "revision": "54a442a15c7b9860ee2e",
+    "url": "/robots-app/static/js/runtime-main.55168987.js"
+  }
+]);
